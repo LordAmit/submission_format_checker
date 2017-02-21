4b6d4f1a39e5bc2ddcf066bfb21b4d33cbd4b5bd
Amit Seal Ami <lordamit@gmail.com>
2017-02-21 23:30:49 +0600

    created logging approach properly.

--- log.py
+++ log.py
@@ -4,9 +4,15 @@ import logging
 import time
 
 def setup_logger(name):
-    
-    filename = time.strftime("%d_%m_%Y") + '.log';
-    logging.basicConfig(filename=filename, level=logging.DEBUG)
+
+    formatter = logging.Formatter(fmt='%(levelname)s - %(module)s - %(funcName)s - %(message)s')
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    return logger
+
 
 
 # def log_this(message):
